3296a8f0272176fb5d1e551d2aa885048026d10b
Ferhaaat <149968227+Ferhaaat@users.noreply.github.com>
2024-02-21 15:04:59 +0100

    add files via upload
    upload 0

---
+++ Acceuil.py
@@ -0,0 +1,29 @@
+import streamlit as st
+
+
+st.title('GymRA : Personnal Coaching with AI')
+
+
+recorded_file = 'output_sample.mp4'
+sample_vid = st.empty()
+sample_vid.video(recorded_file)
+
+
+st.title('GymRA')
+st.header('Parlons un peu de nous :')
+st.text('N')
+
+
+
+st.text('Fixed width text')
+st.markdown('_Markdown_') # see #*
+st.caption('Balloons. Hundreds of them...')
+st.write('Most objects') # df, err, func, keras!
+
+
+# * optional kwarg unsafe_allow_html = True
+    
+
+    
+
+
